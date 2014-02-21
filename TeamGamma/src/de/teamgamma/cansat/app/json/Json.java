d8dc5c653745acf36e34ce76b3f40b2c13cd091b
RobinBley11@gmail.com <RobinBley11@gmail.com@372ab57b-04b0-0044-e8ea-d00faaee71d2>
2014-02-21 08:28:19 +0000

    init

--- TeamGamma/src/de/teamgamma/cansat/app/json/Json.java
+++ TeamGamma/src/de/teamgamma/cansat/app/json/Json.java
@@ -2,41 +2,41 @@ package de.teamgamma.cansat.app.json;
 
 import org.json.JSONObject;
 
+import de.teamgamma.cansat.app.savedata.Save;
+
 public class Json {
-	
+
 	private final String[] names = new String[3];
-	
-	
-	public Json(){
-		this.names[0] = "Time:";
-		this.names[1] = "Temp";
-		this.names[2] = "Co2";
+	private static Json instance = null;
+
+	public static Json getInstance() {
+		if (instance == null) {
+			instance = new Json();
+		}
+		return instance;
+
 	}
-	
-	
-	
-	
-	public void unpack(String json){
-		
+	public Json(){
+		this.names[0] = "time";
+		this.names[1] = "temp";
+		this.names[2] = "co2";
+	}
+
+	public void unpack(String json) {
+
 		JSONObject data = new JSONObject(json);
-		
+
 		/*
 		 * Daten an Save und Values uebergeben.
 		 */
+
+		Save.getInstance().writeAll(data.getLong(names[0]),
+				data.getDouble(names[1]), data.getDouble(names[2]));
+
 		data.getLong(this.names[0]);
 		data.getDouble(names[2]);
 		data.getDouble(names[2]);
-		
-		
-		
-		
-		
+
 	}
-	
-	
-	
-	
-	
-	
 
 }
