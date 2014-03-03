dede46111d32fce181f4e191e5056c8df15cb380
RobinBley11@gmail.com <RobinBley11@gmail.com@372ab57b-04b0-0044-e8ea-d00faaee71d2>
2014-03-03 11:18:41 +0000

    delete old Classes

--- trunk/TeamGamma/src/de/teamgamma/cansat/app/savedata/Read.java
+++ trunk/TeamGamma/src/de/teamgamma/cansat/app/savedata/Read.java
@@ -3,20 +3,16 @@ package de.teamgamma.cansat.app.savedata;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 
-import android.util.Log;
 import de.teamgamma.cansat.app.options.Options;
+
+
 
 public class Read {
 	Options option = Options.getInstance();
 
-	private String filepathtemp;
-	private String filepathco2;
 
-	private double[][] temp;
 	private Double[][] outputArrayList = new Double[20][2];
-	private double[][] co2;
 	private static Read instance = null;
 
 	// Singleton
@@ -27,64 +23,6 @@ public class Read {
 		return instance;
 	}
 
-	public double[][] getTemp(String filepathtemp) {
-		this.filepathtemp = filepathtemp;
-		int counter = 0;
-		String[] lineArray;
-		try {
-			BufferedReader in = new BufferedReader(new FileReader(
-					this.filepathtemp));
-			String zeile = null;
-
-			while ((zeile = in.readLine()) != null) {
-				lineArray = zeile.split(":");
-				for (String i : lineArray) {
-					if (counter % 2 == 0) {
-						temp[counter][0] = Long.parseLong(i);
-					} else {
-						temp[counter][1] = Double.parseDouble(i);
-						counter++;
-					}
-				}
-				in.close();
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		return temp;
-	}
-
-	public double[][] getCo2(String filepathco2) {
-		this.filepathco2 = filepathco2;
-		int counter = 0;
-		String[] lineArray;
-		try {
-			BufferedReader in = new BufferedReader(new FileReader(
-					this.filepathco2));
-			String zeile = null;
-
-			while ((zeile = in.readLine()) != null) {
-				// counter = 0; vielleicht, vielleicht auch nicht
-				lineArray = zeile.split(":");
-				for (String i : lineArray) {
-					if (counter % 2 == 0) {
-						Log.d("i", i); // wird nur einmal aufgerufen, dann
-										// nullPointer in der nächsten Zeile!
-						co2[counter][0] = Long.parseLong(i);
-
-					} else {
-						co2[counter][1] = Double.parseDouble(i);
-						counter++;
-					}
-				}
-				in.close();
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return co2;
-	}
 
 	public Double[][] getValuefromFile(String filepath) {
 		refreshOutputArray();
