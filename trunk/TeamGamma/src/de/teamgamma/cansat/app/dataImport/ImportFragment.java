162fd5ab32132b3446962a636b5ed31fad2c90b3
alexander.brenneck <alexander.brenneck@372ab57b-04b0-0044-e8ea-d00faaee71d2>
2014-03-24 19:44:57 +0000

    added exception when wrong file format was selected to import

--- trunk/TeamGamma/src/de/teamgamma/cansat/app/dataImport/ImportFragment.java
+++ trunk/TeamGamma/src/de/teamgamma/cansat/app/dataImport/ImportFragment.java
@@ -6,6 +6,7 @@ import de.teamgamma.cansat.app.fragments.HomeFragment;
 import de.teamgamma.cansat.app.options.Options;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -17,80 +18,95 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 /**
  * 
  * 
  * @author Alexander Brennecke
- *
- *shows a fragment to select an file which should be displayed in a chart
+ * 
+ *         shows a fragment to select an file which should be displayed in a
+ *         chart
  */
 
 public class ImportFragment extends Fragment {
-	
-	//initialize a few importend variables
+
+	// initialize a few importend variables
 	Options options_data = Options.getInstance();
 	String importFilepath;
 	LinearLayout mLinearLayout;
-	
-	
+
 	/**
-	 * when the fileBrowser finished and the fragment will shown again the onResume will be called
-	 * and update the TextView fields inside the fragment
+	 * when the fileBrowser finished and the fragment will shown again the
+	 * onResume will be called and update the TextView fields inside the
+	 * fragment
 	 */
 	@Override
-	public void onResume(){		
+	public void onResume() {
 		final Options options_data = Options.getInstance();
 		super.onResume();
-		final EditText filePath = (EditText) mLinearLayout.findViewById(R.id.filePath);		
+		final EditText filePath = (EditText) mLinearLayout
+				.findViewById(R.id.filePath);
 		filePath.setText(options_data.getTemporaryBrowserResultPath());
 		importFilepath = options_data.getTemporaryBrowserResultPath();
-		
+
 	}
-	
+
 	/**
 	 * will be called when a new ImportedFragment object is generated
 	 */
 	@Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
-		//initialize a few importend variables
-     	mLinearLayout = (LinearLayout) inflater.inflate(R.layout.fragment_import,container, false);
-     	Button buttonImport = (Button) mLinearLayout.findViewById(R.id.button_import);
-     	Button buttonBrowser = (Button) mLinearLayout.findViewById(R.id.button_browser);
-     	
-     	// ActionListener to open the fileBrowser
-     	buttonBrowser.setOnClickListener(new OnClickListener() {
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// initialize a few importend variables
+		mLinearLayout = (LinearLayout) inflater.inflate(
+				R.layout.fragment_import, container, false);
+		Button buttonImport = (Button) mLinearLayout
+				.findViewById(R.id.button_import);
+		Button buttonBrowser = (Button) mLinearLayout
+				.findViewById(R.id.button_browser);
+
+		// ActionListener to open the fileBrowser
+		buttonBrowser.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				Intent intent = new Intent(
 						getActivity(),
 						de.teamgamma.cansat.app.filebrowser.AndroidExplorer.class);
 				startActivity(intent);
-				
+
 			}
 		});
-     	
-     	// Button to import the file into a chart and display this chart
-     	buttonImport.setOnClickListener(new OnClickListener() {
+
+		// Button to import the file into a chart and display this chart
+		buttonImport.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-		     	Read reader = Read.getInstance();   		     	
-		    ImportedFiles.getInstance().setLatestFile(reader.getValuefromFile(importFilepath));
-		    // Create new fragment and transaction
-			Fragment newFragment = new ImportSimpleXYChart();
-			FragmentTransaction transaction = getFragmentManager().beginTransaction();
-			
-			transaction.replace(R.id.content_frame, newFragment);
-			transaction.addToBackStack(null);
+				Read reader = Read.getInstance();
+				if (reader.getValuefromFile(importFilepath) == null) {
+					Context context = mLinearLayout.getContext();
+					CharSequence text = "Wrong file format ";
+					int duration = Toast.LENGTH_SHORT;
+					Toast toast = Toast.makeText(context, text, duration);
+					toast.show();
+				}
+				else{
+					ImportedFiles.getInstance().setLatestFile(
+							reader.getValuefromFile(importFilepath));
+					// Create new fragment and transaction
+					Fragment newFragment = new ImportSimpleXYChart();
+					FragmentTransaction transaction = getFragmentManager()
+							.beginTransaction();
 
-			// Commit the transaction
-			transaction.commit();
+					transaction.replace(R.id.content_frame, newFragment);
+					transaction.addToBackStack(null);
 
-				
+					// Commit the transaction
+					transaction.commit();					
+				}
 			}
-		});     	
-     return mLinearLayout;	
+		});
+		return mLinearLayout;
 	}
 
 }
