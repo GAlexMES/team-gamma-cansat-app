c317b9453094c12e2a035384d0657f35f3c56d33
RobinBley11@gmail.com <RobinBley11@gmail.com@372ab57b-04b0-0044-e8ea-d00faaee71d2>
2014-04-24 14:41:43 +0000

    Mögliche JsonBugs gefixxed

--- TeamGamma/src/de/teamgamma/cansat/app/data/DataCoordination.java
+++ TeamGamma/src/de/teamgamma/cansat/app/data/DataCoordination.java
@@ -1,4 +1,3 @@
-
 package de.teamgamma.cansat.app.data;
 
 import android.util.Log;
@@ -14,69 +13,67 @@ import de.teamgamma.cansat.app.sensors.Sensor;
 
 public class DataCoordination {
 
-        private Json json = Json.getInstance();
-        private static DataCoordination instance = null;
-        private Sensor[] sensors = new Sensor[constantValues.names.length];
-        private Options options = Options.getInstance();
+	private Json json = Json.getInstance();
+	private static DataCoordination instance = null;
+	private Sensor[] sensors;
+	private Options options;
 
-    	// an instance of the class is created, so there is no recursive call
-        public static DataCoordination getInstance() {
-                if (instance == null) {
-                        instance = new DataCoordination();
-                }
-                return instance;
-        }
+	// an instance of the class is created, so there is no recursive call
+	public static DataCoordination getInstance() {
+		if (instance == null) {
+			instance = new DataCoordination();
+		}
+		return instance;
+	}
 
-        public DataCoordination() {
-    		// the constructor of the class adds in each index of the sensor array,
-    		// a new object of class sensor and give this the corresponding names
-                for (int i = 0; i < constantValues.names.length; i++) {
-                        sensors[i] = new Sensor();
-                        sensors[i].setName(constantValues.names[i]);
+	public DataCoordination() {
+		// the constructor of the class adds in each index of the sensor array,
+		// a new object of class sensor and give this the corresponding names
+		sensors = new Sensor[constantValues.names.length];
+		options = Options.getInstance();
+		for (int i = 0; i < constantValues.names.length; i++) {
+			sensors[i] = new Sensor();
+			sensors[i].setName(constantValues.names[i]);
 
-                }
-        }
+		}
+	}
 
-        public void coordinateData(String message) {
-        	Log.d("message2",message);
-    		// takes the message from the AndroidClient
-    		// message is unpacked
-                Double[][] data = json.unpack(message);
-               Log.d("json","test");
-                for (int i = 0; i < constantValues.names.length; i++) {
-                		Log.d("json","for");
-                	// the data from the unpacked message will be written in every corresponding Sensor
-                        this.sensors[i].setValues(data[i][0].longValue(), data[i][1]);
-                        Log.d("json",String.valueOf(data[i][1]));
-                }
-                Log.d("name","fiiiiicken");
-                for (Sensor sensor : sensors) {
-                Log.d("name","nochmal fiiiiiiiicken");
-                	
-               Log.d("name",sensor.getName()+"/"+options.getOption(KindOfOption.CHARTVIEW.ordinal(), ChartViewOptions.ACTIVESENSORNAME));
-                	// The SensorArray are the SensorArray passed.
-                        if (sensor.getName().equals(options.getOption(KindOfOption.CHARTVIEW.ordinal(), ChartViewOptions.ACTIVESENSORNAME))) {
-                                if (MainActivity.getCurrentFragment().getClass()
-                                                .equals(RealtimeGraph.class)) {
-                                        ((RealtimeGraph) MainActivity.getCurrentFragment())
-                                                        .onValueChanged(sensor);
-                                }
-                        }
-                }
-                // A new Thread will create and give them the SensorArray to save them parallel.
-                SaveThread saveData = new SaveThread();
-                saveData.setData(data);
-                Thread saveThread = new Thread(saveData);
-                saveThread.start();
+	public void coordinateData(String message) {
+		// takes the message from the AndroidClient
+		// message is unpacked
+		Double[][] data = json.unpack(message);
+		for (int i = 0; i < constantValues.names.length; i++) {
+			// the data from the unpacked message will be written in every
+			// corresponding Sensor
+			this.sensors[i].setValues(data[i][0].longValue(), data[i][1]);
+		}
+		for (Sensor sensor : sensors) {
 
-                // RealtimeGraph.getInstance().onValueChanged(this.sensors);
-        }
+			// The SensorArray are the SensorArray passed.
+			if (sensor.getName().equals(
+					options.getOption(KindOfOption.CHARTVIEW.ordinal(),
+							ChartViewOptions.ACTIVESENSORNAME))) {
+				if (MainActivity.getCurrentFragment().getClass()
+						.equals(RealtimeGraph.class)) {
+					((RealtimeGraph) MainActivity.getCurrentFragment())
+							.onValueChanged(sensor);
+				}
+			}
+		}
+		// A new Thread will create and give them the SensorArray to save them
+		// parallel.
+		SaveThread saveData = new SaveThread();
+		saveData.setData(data);
+		Thread saveThread = new Thread(saveData);
+		saveThread.start();
 
-        public Sensor[] getValuesFromDatabase() {
-        	// the SensorArray is filled with the values from the Database.
-                Database database = Database.getInstance();
-                this.sensors = database.getValuesFromDatabase();
-                return sensors;
-        }
+	}
+
+	public Sensor[] getValuesFromDatabase() {
+		// the SensorArray is filled with the values from the Database.
+		Database database = Database.getInstance();
+		this.sensors = database.getValuesFromDatabase();
+		return sensors;
+	}
 
 }
--- TeamGamma/src/de/teamgamma/cansat/app/json/Json.java
+++ TeamGamma/src/de/teamgamma/cansat/app/json/Json.java
@@ -8,14 +8,17 @@ import de.teamgamma.cansat.app.data.constantValues;
 
 public class Json {
 
+	private Double[][] sensors;
 	private static Json instance = null;
-	private Double[][] sensors = new Double[constantValues.names.length][2];
 
 	public static Json getInstance() {
 		if (instance == null) {
 			instance = new Json();
 		}
 		return instance;
+	}
+	private Json(){
+		sensors = new Double[constantValues.names.length][2];
 	}
 
 	public Double[][] unpack(String json) {
@@ -27,7 +30,12 @@ public class Json {
 			time = data.getLong("time");
 			for (int i = 0; i < constantValues.names.length; i++) {
 				this.sensors[i][0] = Double.valueOf(time - constantValues.firstTimestamp);
-				this.sensors[i][1] = data.getDouble(constantValues.names[i]);
+				try{
+					if (data.getDouble(constantValues.names[i]) != 0){
+						this.sensors[i][1] = data.getDouble(constantValues.names[i]);
+					}
+				}catch(JSONException e){	
+				}
 
 			}
 		} catch (JSONException e) {
